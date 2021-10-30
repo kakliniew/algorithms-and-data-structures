ab355555c3d198591b36e3032617e07d7cb3fe85
kakliniew <238896@student.pwr.edu.pl>
2021-10-30 20:28:19 +0200

    dfs implementation started

--- p1/main.cpp
+++ p1/main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <set>
+#include <stack>
 
 using namespace std;
 
@@ -48,6 +50,62 @@ void printGraphs(vector<vector<vector<int>>> matrixes){
 
 }
 
+int searchFristNotVisitedNeighbor(vector<vector<int>> matrix, set<int> odwiedzone_zielone, set<int> odwiedzone_czerwone, int current_W){
+    for(int i = 1; i < matrix.size(); i++){
+        for(int j = 0; j < 2; j++){
+            if(matrix[i][j] == current_W && !odwiedzone_zielone.count(matrix[i][(j+1)%2]) && !odwiedzone_czerwone.count(matrix[i][(j+1)%2])){
+                return matrix[i][(j+1)%2];
+            }
+        }
+    }
+    return 0;
+}
+
+void doDfs(vector<vector<int>> matrix){
+    int liczba_w = matrix[0][0];
+    int liczba_k = matrix[0][1];
+    set<int> odwiedzone_zielone;
+    set<int> odwiedzone_czerwone;
+    int current_W = matrix[1][0];
+    stack<int> dfsStack;
+    dfsStack.push(0);
+    dfsStack.push(current_W);
+    odwiedzone_czerwone.insert(current_W);
+    int previosW;
+    while(odwiedzone_czerwone.size() + odwiedzone_czerwone.size() < liczba_w){
+        int znaleziony_sasiad = 0;
+        if(znaleziony_sasiad = searchFristNotVisitedNeighbor(matrix, odwiedzone_zielone, odwiedzone_czerwone, current_W)){
+            cout<<"Znaleziony sasiad " << znaleziony_sasiad << endl;
+            dfsStack.push(znaleziony_sasiad);
+            if(odwiedzone_czerwone.count(current_W)){
+                odwiedzone_zielone.insert(znaleziony_sasiad);
+            } else{
+                odwiedzone_czerwone.insert(znaleziony_sasiad);
+            }
+            previosW = current_W;
+            current_W = znaleziony_sasiad;
+            
+        }else if(current_W == 0){
+            cout<<"koniec"<<endl;
+            break;
+        }
+        else{
+            cout<< current_W <<" nie ma kolejnego nieodwiedzonego sasiada"<<endl;
+            dfsStack.pop();
+            current_W = dfsStack.top();
+            dfsStack.pop();
+            previosW = dfsStack.top();
+            dfsStack.push(current_W);
+            cout<<"Powrot do " << current_W << " z poprzednim " << previosW << endl;
+         
+            
+        }
+
+    }
+    cout<<"KONIEC: znaleziono " << odwiedzone_czerwone.size() + odwiedzone_czerwone.size() << " z " << liczba_w << endl;
+    
+}
+
 vector<vector<vector<int>>> loadFromStdIn(){
     vector<vector<vector<int>>> matrixes;
     int liczba_zestawow;
@@ -79,7 +137,7 @@ vector<vector<vector<int>>> loadFromStdIn(){
 int main(){
     vector<vector<vector<int>>> matrixes = loadFromFile("dane.txt");
     printGraphs(matrixes);
-     vector<vector<vector<int>>> matrixes2 = loadFromStdIn();
+    doDfs(matrixes[1]);
 
     return 0;
 }
